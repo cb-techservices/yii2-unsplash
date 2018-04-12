41e815ce37e6bcaf6cc4a5953939d28e3a9debdc
carlb <burnstei@uoregon.edu>
2018-04-12 13:35:13 -0700

    -Setting up project dir. added gitignore for composer files. Created first Widget class.

---
+++ UnsplashWidget.php
@@ -0,0 +1,24 @@
+<?php 
+
+namespace cbtech\unsplash;
+
+use yii\base\Widget;
+use yii\helpers\Html;
+
+class HelloWidget extends Widget
+{
+    public $message;
+
+    public function init()
+    {
+        parent::init();
+        if ($this->message === null) {
+            $this->message = 'Hello World';
+        }
+    }
+
+    public function run()
+    {
+        return Html::encode($this->message);
+    }
+}
